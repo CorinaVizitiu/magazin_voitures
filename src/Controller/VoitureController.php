3b7bc243078476cfa5a4ae0b90248d3877679fcc
CorinaVizitiu <corina.vizitiu@yahoo.com>
2023-05-24 11:43:21 +0200

    projet voiture

---
+++ src/Controller/VoitureController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Voiture;
+use App\Form\VoitureType;
+use App\Repository\VoitureRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class VoitureController extends AbstractController
+{
+    #[Route('/', name: 'app_voiture_index', methods: ['GET'])]
+    public function index(VoitureRepository $voitureRepository): Response
+    {
+        return $this->render('voiture/index.html.twig', [
+            'voitures' => $voitureRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/voiture/creer', name: 'app_voiture_creer', methods: ['GET', 'POST'])]
+    public function creer(Request $request, VoitureRepository $voitureRepository): Response
+    {
+        $voiture = new Voiture();
+        $form = $this->createForm(VoitureType::class, $voiture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $voitureRepository->save($voiture, true);
+            $this->addFlash('success', 'Voiture  successfully cree !');
+            return $this->redirectToRoute('app_voiture_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('voiture/creer.html.twig', [
+            'voiture' => $voiture,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/voiture/{id}', name: 'app_voiture_details', methods: ['GET'])]
+    public function details(Voiture $voiture): Response
+    {
+        return $this->render('voiture/details.html.twig', [
+            'voiture' => $voiture,
+        ]);
+    }
+
+    #[Route('/voiture/{id}/editer', name: 'app_voiture_editer', methods: ['GET', 'POST'])]
+    public function editer(Request $request, Voiture $voiture, VoitureRepository $voitureRepository): Response
+    {
+        $form = $this->createForm(VoitureType::class, $voiture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $voitureRepository->save($voiture, true);
+            $this->addFlash('success', 'Voiture successfully editer !');
+            return $this->redirectToRoute('app_voiture_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('voiture/editer.html.twig', [
+            'voiture' => $voiture,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_voiture_supprimer', methods: ['POST'])]
+    public function supprimer(Request $request, Voiture $voiture, VoitureRepository $voitureRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$voiture->getId(), $request->request->get('_token'))) {
+            $voitureRepository->remove($voiture, true);
+        }
+       
+        $this->addFlash('info', 'Voiture  successfully supprime!'); 
+        return $this->redirectToRoute('app_voiture_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Voiture.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoitureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoitureRepository::class)]
+#[ORM\Table(name: "voitures")]
+#[ORM\HasLifecycleCallbacks]
+class Voiture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $marque = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $couleur = null;
+
+    #[ORM\Column]
+    private ?int $annee = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): self
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getCouleur(): ?string
+    {
+        return $this->couleur;
+    }
+
+    public function setCouleur(string $couleur): self
+    {
+        $this->couleur = $couleur;
+
+        return $this;
+    }
+
+    public function getAnnee(): ?int
+    {
+        return $this->annee;
+    }
+
+    public function setAnnee(int $annee): self
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+}
